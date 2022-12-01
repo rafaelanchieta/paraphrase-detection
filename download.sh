6f4b615e9e4b1fb6ff2f6acb6bf9f95aa44d9717
rafaelanchieta <lynux100@gmail.com>
2022-12-01 14:15:41 -0300

    Download pre-trained embeddings

--- download.sh
+++ download.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-wget http://143.107.183.175:22980/download.php?file=embeddings/word2vec/skip_s300.zip
+# wget http://143.107.183.175:22980/download.php?file=embeddings/word2vec/skip_s300.zip
 mkdir -p "models"
-unzip skip_s300.zip -d /models
-rm skip_s300.zip
+wget liara.picos.ifpi.edu.br/embeddings
+mv embeddings models/
+wget liara.picos.ifpi.edu.br/embeddings.vectors.npy
+mv embeddings.vectors.npy models/
+# unzip skip_s300.zip -d /models
+# rm skip_s300.zip
 
 echo "Done!!"
